8c177e6bced9f9e3cff038163954333dfb80f21b
Jose Sousa <2409jmsousa@gmail.com>
2025-01-04 16:44:15 +0000

    now board row is cleaner

--- game.pl
+++ game.pl
@@ -15,10 +15,10 @@ play:-
     game_cycle(GameState-Player-NextPlayer-Variant, GameMode).
 
 
-
 %initial_state(+GameConfig, -GameState)
 %takes as input GameConfig with the specified mode and board size and outputs the initial gamestate
 %the game state consists of Board 2D list , curplayer, nextplayer and variant
+
 initial_state(GameMode-Size, Board-Player-NextPlayer-Variant):-
     initial_board(Size,Board),
     initial_players(GameMode, Player,NextPlayer),
@@ -52,120 +52,35 @@ level_of_ai(computer1_hard,2).
 level_of_ai(computer2_easy,1).
 level_of_ai(computer2_hard,2).
 
-initial_board(Size,Board):-
-    init_board(Size, Board).
 
-get_board_size(ValidSize):-
-    write('Enter board size (even number between 4 and 26):'), nl,
-    catch(read(Size), _, (write('Read error. This may cause the next reads to fail.'), nl, get_board_size(Size))),
-    validate_size(Size, ValidSize).
 
-validate_size(Size, ValidSize) :-
-    integer(Size),
-    Size >= 4,
-    Size =< 26,
-    Size mod 2 =:= 0,
-    !,
-    ValidSize = Size.
+initial_board(Size, Board) :-
+    findall(Row,(between(1,Size,X), initial_board_row(X,Size,Row)),Board). 
 
-validate_size(_, ValidSize) :-
-    write('Invalid input.'), nl,
-    get_board_size(ValidSize).
-
-init_board(Size, Board) :-
-    init_board(Size, 1, [], Board). 
-
-init_board(Size, Row, AccumulatedBoard, Board) :-
-    Row =< Size,
-    init_board_row(Row, Size, BoardRow),
-    append(AccumulatedBoard, [BoardRow], NewAccumulatedBoard),
-    NextRow is Row + 1,
-    init_board(Size, NextRow, NewAccumulatedBoard, Board).
-
-init_board(Size, Row, Board, Board) :-
-    Row > Size.
-
-init_board_row(Row, Size, BoardRow) :-
-    init_board_row(Row, 1, Size, [], BoardRowHelper),
-    reverse(BoardRowHelper, BoardRow).
+initial_board_row(Row, Size, BoardRow) :-
+    findall(Piece,(between(1,Size,X),initial_board_cell(Row,X,Piece,Size)),BoardRow).
     
-
-% Helper predicate to accumulate cells for each column in the row
-init_board_row(_, Col, Size, Acc, Acc) :- 
-    Col > Size. 
-init_board_row(Row, Col, Size, Acc, BoardRow) :-
-    init_board_cell(Row, Col, BoardCell, Size),  
-    NewCol is Col + 1, 
-    init_board_row(Row, NewCol, Size, [BoardCell|Acc], BoardRow).
-
-init_board_cell(1,Y, red,Size) :-
+initial_board_cell(1,Y, red,Size) :-
     Y > 1,
     Y < Size,
     Y mod 2 =:= 1,!.
 
-init_board_cell(Size,Y, red,Size) :-
+initial_board_cell(Size,Y, red,Size) :-
     Y > 1,
     Y < Size,
     Y mod 2 =:= 0,!.
 
-init_board_cell(X,1, blue,Size) :-
+initial_board_cell(X,1, blue,Size) :-
     X > 1,
     X < Size,
     X mod 2 =:= 0,!.
 
-init_board_cell(X,Size, blue,Size) :-
+initial_board_cell(X,Size, blue,Size) :-
     X > 1,
     X < Size,
     X mod 2 =:= 1,!.
 
-init_board_cell(_,_, empty,_).
-
-get_variant(Variant):-
-    write('Choose variant:'), nl,
-    write('1. Standard'), nl,
-    write('2. Medium Churn'), nl,
-    write('3. High Churn'), nl,
-    catch(read(Variant_Chosen), _, (write('Read error. This may cause the next reads to fail.'), nl, get_variant(Variant_Chosen))),
-    validate_variant(Variant_Chosen, Variant).
-
-validate_variant(Variant_Chosen, Variant) :-
-    member(Variant_Chosen, [1, 2, 3]),
-    !,
-    Variant = Variant_Chosen.
-validate_variant(_, Variant) :-
-    nl, write('Invalid variant chosen.'), nl, nl,
-    get_variant(Variant).
-
-get_game_mode(GameMode):-
-    write('Choose game mode:'), nl,
-    write('1. Player vs Player'), nl,
-    write('2. Player vs Computer'), nl,
-    write('3. Computer vs Player'), nl,
-    write('4. Computer vs Computer'), nl,
-    catch(read(Mode), _, (write('Read error. This may cause the next reads to fail.'), nl, get_game_mode(Mode))),
-    validate_mode(Mode, GameMode).
-
-validate_mode(Mode, GameMode) :-
-    member(Mode, [1, 2, 3, 4]),
-    !,
-    GameMode = Mode.
-validate_mode(_, GameMode) :-
-    nl, write('Invalid mode chosen.'), nl, nl,
-    get_game_mode(GameMode).
-
-get_AI_level(Level):-
-    write('Choose AI level:'), nl,
-    write('1. Random'), nl,
-    write('2. Greedy'), nl,
-    catch(read(AI), _, (write('Read error. This may cause the next reads to fail.'), nl, get_AI_level(AI))),
-    validate_AI(AI, Level).
-validate_AI(AI, Level) :-
-    member(AI, [1, 2]),
-    !,
-    Level = AI.
-validate_AI(_, Level) :-
-    nl, write('Invalid AI level chosen.'), nl, nl,
-    get_AI_level(Level).
+initial_board_cell(_,_, empty,_).
 
 
 game_cycle(GameState-Player-_-_,GameMode):-
@@ -498,52 +413,4 @@ choose_move(GameState-computer2_hard-_-Variant,2, Move):-
     random_select(Move,GoodMoves,_Rest),
     inverse_transform_move(Move, TransformedMove),
     nl, write('Blue Computer (greedy) chose move: '), write(TransformedMove), nl.
-
-inverse_transform_move(StartColNum-StartRowNum-EndColNum-EndRowNum, Start-End) :-
-    num_to_col(StartColNum, StartCol),
-    num_to_col(EndColNum, EndCol),
-    number_codes(StartRowNum, StartRowCodes),
-    number_codes(EndRowNum, EndRowCodes),
-    atom_codes(StartRow, StartRowCodes),
-    atom_codes(EndRow, EndRowCodes),
-    atom_concat(StartCol, StartRow, Start),
-    atom_concat(EndCol, EndRow, End).
-
-num_to_col(Num, Col) :-
-    Code is Num + 96, % 1 -> 'a'
-    char_code(Col, Code).
-
-
-get_move(Move) :-
-    nl, write('Enter move (e.g., b1-c3): '), nl,
-    catch(read(InputMove), _, (nl, write('Read error. This may cause the next reads to fail.'), nl, fail)),
-    validate_move_format(InputMove),
-    transform_move(InputMove, TransformedMove),
-    Move = TransformedMove.
-
-validate_move_format(Start-End) :-
-    atom_chars(Start, StartChars), % Posição inicial
-    atom_chars(End, EndChars),     % Posição final
-    validate_position_format(StartChars),
-    validate_position_format(EndChars).
-
-validate_position_format([Col|RowChars]) :-
-    char_code(Col, ColCode),
-    ColCode >= 97, ColCode =< 122, % coluna é uma letra
-    maplist(char_code, RowChars, RowCodes),
-    maplist(between(48, 57), RowCodes). % linha é um número
-
-transform_move(Start-End, StartColNum-StartRowNum-EndColNum-EndRowNum) :-
-    atom_chars(Start, [StartCol|StartRowChars]), % Separar coluna e linha
-    atom_chars(End, [EndCol|EndRowChars]),       % Separar coluna e linha
-    col_to_num(StartCol, StartColNum),
-    col_to_num(EndCol, EndColNum),
-    maplist(char_code, StartRowChars, StartRowCodes),
-    maplist(char_code, EndRowChars, EndRowCodes),
-    number_codes(StartRowNum, StartRowCodes), % Converter string para número
-    number_codes(EndRowNum, EndRowCodes).     % Converter string para número
-
-col_to_num(Col, Num) :-
-    char_code(Col, Code),
-    Num is Code - 96. % 'a' -> 1
 
--- interface.pl
+++ interface.pl
@@ -115,3 +115,119 @@ congratulate(blue) :-
     nl,
     write('\e[34mCongratulations, Blue!'),
     write('\e[0m'), nl, nl.
+
+
+get_board_size(ValidSize):-
+    write('Enter board size (even number between 4 and 26):'), nl,
+    catch(read(Size), _, (write('Read error. This may cause the next reads to fail.'), nl, get_board_size(Size))),
+    validate_size(Size, ValidSize).
+
+
+validate_size(Size, ValidSize) :-
+    integer(Size),
+    Size >= 4,
+    Size =< 26,
+    Size mod 2 =:= 0,
+    !,
+    ValidSize = Size.
+
+validate_size(_, ValidSize) :-
+    write('Invalid input.'), nl,
+    get_board_size(ValidSize).
+
+
+get_variant(Variant):-
+    write('Choose variant:'), nl,
+    write('1. Standard'), nl,
+    write('2. Medium Churn'), nl,
+    write('3. High Churn'), nl,
+    catch(read(Variant_Chosen), _, (write('Read error. This may cause the next reads to fail.'), nl, get_variant(Variant_Chosen))),
+    validate_variant(Variant_Chosen, Variant).
+
+validate_variant(Variant_Chosen, Variant) :-
+    member(Variant_Chosen, [1, 2, 3]),
+    !,
+    Variant = Variant_Chosen.
+validate_variant(_, Variant) :-
+    nl, write('Invalid variant chosen.'), nl, nl,
+    get_variant(Variant).
+
+get_game_mode(GameMode):-
+    write('Choose game mode:'), nl,
+    write('1. Player vs Player'), nl,
+    write('2. Player vs Computer'), nl,
+    write('3. Computer vs Player'), nl,
+    write('4. Computer vs Computer'), nl,
+    catch(read(Mode), _, (write('Read error. This may cause the next reads to fail.'), nl, get_game_mode(Mode))),
+    validate_mode(Mode, GameMode).
+
+validate_mode(Mode, GameMode) :-
+    member(Mode, [1, 2, 3, 4]),
+    !,
+    GameMode = Mode.
+validate_mode(_, GameMode) :-
+    nl, write('Invalid mode chosen.'), nl, nl,
+    get_game_mode(GameMode).
+
+get_AI_level(Level):-
+    write('Choose AI level:'), nl,
+    write('1. Random'), nl,
+    write('2. Greedy'), nl,
+    catch(read(AI), _, (write('Read error. This may cause the next reads to fail.'), nl, get_AI_level(AI))),
+    validate_AI(AI, Level).
+validate_AI(AI, Level) :-
+    member(AI, [1, 2]),
+    !,
+    Level = AI.
+validate_AI(_, Level) :-
+    nl, write('Invalid AI level chosen.'), nl, nl,
+    get_AI_level(Level).
+
+
+get_move(Move) :-
+    nl, write('Enter move (e.g., b1-c3): '), nl,
+    catch(read(InputMove), _, (nl, write('Read error. This may cause the next reads to fail.'), nl, fail)),
+    validate_move_format(InputMove),
+    transform_move(InputMove, TransformedMove),
+    Move = TransformedMove.
+
+validate_move_format(Start-End) :-
+    atom_chars(Start, StartChars), % Posição inicial
+    atom_chars(End, EndChars),     % Posição final
+    validate_position_format(StartChars),
+    validate_position_format(EndChars).
+
+validate_position_format([Col|RowChars]) :-
+    char_code(Col, ColCode),
+    ColCode >= 97, ColCode =< 122, % coluna é uma letra
+    maplist(char_code, RowChars, RowCodes),
+    maplist(between(48, 57), RowCodes). % linha é um número
+
+transform_move(Start-End, StartColNum-StartRowNum-EndColNum-EndRowNum) :-
+    atom_chars(Start, [StartCol|StartRowChars]), % Separar coluna e linha
+    atom_chars(End, [EndCol|EndRowChars]),       % Separar coluna e linha
+    col_to_num(StartCol, StartColNum),
+    col_to_num(EndCol, EndColNum),
+    maplist(char_code, StartRowChars, StartRowCodes),
+    maplist(char_code, EndRowChars, EndRowCodes),
+    number_codes(StartRowNum, StartRowCodes), % Converter string para número
+    number_codes(EndRowNum, EndRowCodes).     % Converter string para número
+
+col_to_num(Col, Num) :-
+    char_code(Col, Code),
+    Num is Code - 96. % 'a' -> 1
+
+inverse_transform_move(StartColNum-StartRowNum-EndColNum-EndRowNum, Start-End) :-
+    num_to_col(StartColNum, StartCol),
+    num_to_col(EndColNum, EndCol),
+    number_codes(StartRowNum, StartRowCodes),
+    number_codes(EndRowNum, EndRowCodes),
+    atom_codes(StartRow, StartRowCodes),
+    atom_codes(EndRow, EndRowCodes),
+    atom_concat(StartCol, StartRow, Start),
+    atom_concat(EndCol, EndRow, End).
+
+num_to_col(Num, Col) :-
+    Code is Num + 96, % 1 -> 'a'
+    char_code(Col, Code).
+
