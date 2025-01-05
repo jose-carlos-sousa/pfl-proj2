1271c5d2916298ed5bcb3542947acd85a583b585
Jose Sousa <2409jmsousa@gmail.com>
2025-01-05 11:13:35 +0000

    cleanup

--- board.pl
+++ board.pl
@@ -48,23 +48,23 @@ initial_board_cell(X,Size, Size, blue) :-
 initial_board_cell(_,_,_, empty).
 
 %helper predicate that given a board replaces all pieces of color1 by color2 and gives new state
-replace_pieces(GameState, Color1, Color2, NewGameState):-
-    length(GameState, L),
-    replace_pieces(GameState, 1-1, L-L, Color1, Color2, NewGameState).
+replace_pieces(Board, Color1, Color2, NewBoard):-
+    length(Board, L),
+    replace_pieces(Board, 1-1, L-L, Color1, Color2, NewBoard).
 
-replace_pieces(GameState, C-L, C-L, Color1, Color2, NewGameState):-
-    get_piece(GameState, C-L, Color1),
-    set_piece(GameState, C-L, Color2, NewGameState), !.
-replace_pieces(GameState, C-L, C-L, _ , _ ,GameState).
+replace_pieces(Board, C-L, C-L, Color1, Color2, NewBoard):-
+    get_piece(Board, C-L, Color1),
+    set_piece(Board, C-L, Color2, NewBoard), !.
+replace_pieces(Board, C-L, C-L, _ , _ ,Board).
   
-replace_pieces(GameState, C-L, C1-L1, Color1, Color2, NewGameState):-
-    get_piece(GameState, C-L, Color1),
-    set_piece(GameState, C-L, Color2, TempGameState),
+replace_pieces(Board, C-L, C1-L1, Color1, Color2, NewBoard):-
+    get_piece(Board, C-L, Color1),
+    set_piece(Board, C-L, Color2, TempBoard),
     next_position(C-L, C1-L1, NextC-NextL),
-    replace_pieces(TempGameState, NextC-NextL, C1-L1, Color1, Color2, NewGameState), !.
-replace_pieces(GameState, C-L, C1-L1, Color1, Color2, NewGameState):-
+    replace_pieces(TempBoard, NextC-NextL, C1-L1, Color1, Color2, NewBoard), !.
+replace_pieces(Board, C-L, C1-L1, Color1, Color2, NewBoard):-
     next_position(C-L, C1-L1, NextC-NextL),
-    replace_pieces(GameState, NextC-NextL, C1-L1, Color1, Color2, NewGameState).
+    replace_pieces(Board, NextC-NextL, C1-L1, Color1, Color2, NewBoard).
 
 %replaces the element at a specified index(I) in a list with a new value
 replace(1, [_|T], X, [X|T]).
@@ -92,7 +92,7 @@ adjacent(C-L, C2-L2):-
     C2 is C - 1, L2 is L + 1.
 
 %computes the next position on a board, either moving to the next column in the current row or advancing to the first column of the next row 
-next_position(C-L, C-L2, NextC-NextL):-
+next_position(C-L, C-_, NextC-NextL):-
     NextL is L + 1,
     NextC is 1.
 next_position(C-L, _-_, NextC-NextL):-
@@ -100,38 +100,38 @@ next_position(C-L, _-_, NextC-NextL):-
     NextL is L.
 
 %gives the adjancent pieces to a given place(C-L)
-adjacent_stones(GameState, C-L, Stones):-
+adjacent_stones(Board, C-L, Stones):-
     findall(Piece, 
             (   adjacent(C-L, C2-L2),                 % Get adjacent coordinates
-                get_piece(GameState, C2-L2, Piece)),   % Get the piece at the adjacent position
+                get_piece(Board, C2-L2, Piece)),   % Get the piece at the adjacent position
             Stones).
-adjacent_black_stones_coordinates(GameState, C-L, Stones):-
+adjacent_black_stones_coordinates(Board, C-L, Stones):-
     findall(C2-L2, 
             (   adjacent(C-L, C2-L2),                 % Get adjacent coordinates
-                get_piece(GameState, C2-L2, black)),   % Get the piece at the adjacent position
+                get_piece(Board, C2-L2, black)),   % Get the piece at the adjacent position
             Stones).
 
 %helper predicate that gets the Piece in a given place(C-L) of the board
-get_piece(GameState, C-L, Piece):-
-    nth1(L, GameState, Row),
+get_piece(Board, C-L, Piece):-
+    nth1(L, Board, Row),
     nth1(C, Row, Piece).
 
 %helper predicate that finds all the pieces of a given player in the board
-find_player_pieces(GameState, Player, Coordinates) :-
+find_player_pieces(Board, Player, Coordinates) :-
     findall(C-L, (
-        nth1(L, GameState, Row),
+        nth1(L, Board, Row),
         nth1(C, Row, Player)
     ), Coordinates).
 
 %helper predicate that updates the board by setting a place(C-L) to a desired piece
-set_piece(GameState, C-L, Piece, NewGameState):-
-    nth1(L, GameState, Row),
+set_piece(Board, C-L, Piece, NewBoard):-
+    nth1(L, Board, Row),
     replace(C, Row, Piece, NewRow),
-    replace(L, GameState, NewRow, NewGameState).
+    replace(L, Board, NewRow, NewBoard).
 
 %predicate that checks board has blue stones left
 there_are_blue_left([]):- fail.
-there_are_blue_left([CurRow|OtherRows]):-
+there_are_blue_left([CurRow|_]):-
     member(blue, CurRow), !.
 there_are_blue_left([CurRow|OtherRows]):-
     \+ member(blue, CurRow),
@@ -139,7 +139,7 @@ there_are_blue_left([CurRow|OtherRows]):-
 
 %predicate that checks board has red stones left
 there_are_red_left([]):- fail.
-there_are_red_left([CurRow|OtherRows]):-
+there_are_red_left([CurRow|_]):-
     member(red, CurRow), !.
 there_are_red_left([CurRow|OtherRows]):-
     \+ member(red, CurRow),
--- move.pl
+++ move.pl
@@ -7,33 +7,26 @@ Medium -> Adjacent Black Stones
 High -> All Black Stones
 
 */
-remove_blocked_stones(Variant,[H|T], NewGameState):-
+remove_blocked_stones(Variant,[H|T], NewBoard):-
     length(H, NumCols),
     length([H|T], NumRows),
-    remove_blocked_stones_helper(Variant,1-1, NumCols-NumRows, [H|T],[H|T], NewGameState) , !.
+    remove_blocked_stones_helper(Variant,1-1, NumCols-NumRows, [H|T],[H|T], NewBoard) , !.
 
 % has original board board accumulator and final 
-remove_blocked_stones_helper(Variant,C-L, C-L, GameState,GameStateAcc, NewGameState):- 
-    remove_blocked_stones_piece(Variant,GameState,GameStateAcc, C-L, NewGameState).
+remove_blocked_stones_helper(Variant,C-L, C-L, Board,BoardAcc, NewBoard):- 
+    remove_blocked_stones_piece(Variant,Board,BoardAcc, C-L, NewBoard).
 
-remove_blocked_stones_helper(Variant,C-L, C2-L2, GameState,GameStateAcc, NextGameState):-
-    remove_blocked_stones_piece(Variant,GameState,GameStateAcc, C-L, NewGameState),
+remove_blocked_stones_helper(Variant,C-L, C2-L2, Board,BoardAcc, NextBoard):-
+    remove_blocked_stones_piece(Variant,Board,BoardAcc, C-L, NewBoard),
     next_position(C-L, C2-L2, NextC-NextL),
-    remove_blocked_stones_helper(Variant,NextC-NextL, C2-L2,GameState ,NewGameState, NextGameState).
+    remove_blocked_stones_helper(Variant,NextC-NextL, C2-L2,Board ,NewBoard, NextBoard).
 
 
 
-% if piece is empty nothing should be done
-remove_blocked_stones_piece(_,GameState,GameAcc,C-L, GameAcc):-
-    get_piece(GameState, C-L, empty), !.
-
-% if piece is black nothing should be done
-remove_blocked_stones_piece(_,GameState,GameAcc,C-L, GameAcc):-
-    get_piece(GameState, C-L, black), !.
 
 /*
 
-remove_blocked_stones_piece(+Variant, +Board, +GameAcc, +C-L, -NewGameState )
+remove_blocked_stones_piece(+Variant, +Board, +GameAcc, +C-L, -NewBoard )
 
 Get Piece
 Get adjacents
@@ -45,57 +38,63 @@ For High Churn:  set the piece to empty && Remove all black pieces in the board
 
 */
 
-remove_blocked_stones_piece(1, GameState,GameAcc,C-L, NewGameState ):-
-    get_piece(GameState, C-L, Piece),
-    adjacent_stones(GameState, C-L, Stones),
-    \+ member(empty, Stones),
-    set_piece(GameAcc, C-L, empty, NewGameState),!.
+% if piece is empty nothing should be done
+remove_blocked_stones_piece(_,Board,GameAcc,C-L, GameAcc):-
+    get_piece(Board, C-L, empty), !.
 
-remove_blocked_stones_piece(2, GameState,GameAcc,C-L, NewGameState ):-
-    get_piece(GameState, C-L, Piece),
-    adjacent_stones(GameState, C-L, Stones),
+% if piece is black nothing should be done
+remove_blocked_stones_piece(_,Board,GameAcc,C-L, GameAcc):-
+    get_piece(Board, C-L, black), !.
+
+remove_blocked_stones_piece(1, Board,GameAcc,C-L, NewBoard ):-
+    adjacent_stones(Board, C-L, Stones),
+    \+ member(empty, Stones),
+    set_piece(GameAcc, C-L, empty, NewBoard),!.
+
+remove_blocked_stones_piece(2, Board,GameAcc,C-L, NewBoard ):-
+    adjacent_stones(Board, C-L, Stones),
     \+ member(empty, Stones),
     set_piece(GameAcc, C-L, empty, NewBoard1),
-    remove_all_black_neighbours(NewBoard1, C-L, NewGameState), !.
+    remove_all_black_neighbours(NewBoard1, C-L, NewBoard), !.
 
-remove_blocked_stones_piece(3, GameState,GameAcc,C-L, NewGameState ):-
-    get_piece(GameState, C-L, Piece),
-    adjacent_stones(GameState, C-L, Stones),
+remove_blocked_stones_piece(3, Board,GameAcc,C-L, NewBoard ):-
+    adjacent_stones(Board, C-L, Stones),
     \+ member(empty, Stones),
     set_piece(GameAcc, C-L, empty, NewBoard1),
-    remove_all_black(NewBoard1, NewGameState), !.
+    remove_all_black(NewBoard1, NewBoard), !.
 
-%used for high churn removes all black pieces fromt the board and replaces them with empty
-remove_all_black(GameState, NewBoard):-
-    replace_pieces(GameState, black, empty,NewBoard).
-
-
-remove_blocked_stones_piece( Variant,GameState,GameAcc,C-L, GameAcc):-
-    get_piece(GameState, C-L, Piece),
-    adjacent_stones(GameState, C-L, Stones),
+remove_blocked_stones_piece( _,Board,GameAcc,C-L, GameAcc):-
+    adjacent_stones(Board, C-L, Stones),
     member(empty, Stones) , !.
 
-%used for Medium Churn, recursively removes all neighboring black stones from the board
-remove_all_black_neighbours(GameState, C-L, NewBoard):-
-    adjacent_black_stones_coordinates(GameState, C-L, Stones),
-    remove_all_black_neighbours(GameState, Stones, NewBoard).
+%used for high churn removes all black pieces fromt the board and replaces them with empty
+remove_all_black(Board, NewBoard):-
+    replace_pieces(Board, black, empty,NewBoard).
 
-remove_all_black_neighbours(GameState, [], GameState).
-remove_all_black_neighbours(GameState, [C-L|OtherStones], NewBoard):-
-   set_piece(GameState, C-L, empty, TempBoard),
+
+
+
+%used for Medium Churn, recursively removes all neighboring black stones from the board
+remove_all_black_neighbours(Board, C-L, NewBoard):-
+    adjacent_black_stones_coordinates(Board, C-L, Stones),
+    remove_all_black_neighbours(Board, Stones, NewBoard).
+
+remove_all_black_neighbours(Board, [], Board).
+remove_all_black_neighbours(Board, [C-L|OtherStones], NewBoard):-
+   set_piece(Board, C-L, empty, TempBoard),
     remove_all_black_neighbours(TempBoard, OtherStones, NewBoard).
 
 %checks if move is valid queen move
-check_move(GameState-Player,Move):-
-    player_has_piece(GameState-Player, Move),
-    is_destination_empty(GameState,Move),
+check_move(Board-Player,Move):-
+    player_has_piece(Board-Player, Move),
+    is_destination_empty(Board,Move),
     valid_direction(Move),
-    path_is_clear(GameState, Move), !.
+    path_is_clear(Board, Move), !.
     
 %checks if direction is either horizontal,vertical or diagonal 
-valid_direction(C1-L1-C2-L2):-
+valid_direction(C1-_-C2-_):-
     C1 =:= C2.
-valid_direction(C1-L1-C2-L2):-
+valid_direction(_-L1-_-L2):-
     L1 =:= L2.
 valid_direction(C1-L1-C2-L2):-
     C1 - C2 =:= L1 - L2.
@@ -103,15 +102,15 @@ valid_direction(C1-L1-C2-L2):-
     C1 - C2 =:= L2 - L1.
     
 %checks if there is no non empty stone in the way, uses next to get the next stone to visit while doing the path
-path_is_clear(GameState, C1-L1-C2-L2):-
-    path_is_clear(GameState, C1-L1-C2-L2, C1, L1).
-path_is_clear(GameState, C1-L1-C2-L2, C, L):-
+path_is_clear(Board, C1-L1-C2-L2):-
+    path_is_clear(Board, C1-L1-C2-L2, C1, L1).
+path_is_clear(_, _-_-C2-L2, C, L):-
     C =:= C2, L =:= L2.
-path_is_clear(GameState, C1-L1-C2-L2, C, L):-
+path_is_clear(Board, C1-L1-C2-L2, C, L):-
     next(C, C2, NextC),
     next(L, L2, NextL),
-    get_piece(GameState, NextC-NextL, empty),
-    path_is_clear(GameState, C1-L1-C2-L2, NextC, NextL).
+    get_piece(Board, NextC-NextL, empty),
+    path_is_clear(Board, C1-L1-C2-L2, NextC, NextL).
 
 %if C2 is bigger, increase C
 next(C, C2, NextC):-
@@ -138,15 +137,15 @@ next(L, L2, L):-
     L =:= L2.
 
 %checks if there is no non empty stone in the destination
-is_destination_empty(GameState, C1-L1-C2-L2):-
-    get_piece(GameState, C2-L2, empty).
+is_destination_empty(Board, _-_-C2-L2):-
+    get_piece(Board, C2-L2, empty).
 
 %for all pieces of a given color gets list of the ratios LenEmpt/LenTot sums values and returns negative is used for calculating state value
-ratio_surrounding_color(GameState, Color, Num):-
+ratio_surrounding_color(Board, Color, Num):-
     findall(Ratio,(
-            get_piece(GameState,C-L,Color),
-            adjacent_stones(GameState, C-L, Stones),
-            findall(Emp, (member(empty,Stones)), Empties),
+            get_piece(Board,C-L,Color),
+            adjacent_stones(Board, C-L, Stones),
+            findall(_, (member(empty,Stones)), Empties),
             length(Empties,LenEmpt),
             length(Stones, LenTot),
             Ratio is LenEmpt/LenTot),Ratios
@@ -155,31 +154,31 @@ ratio_surrounding_color(GameState, Color, Num):-
     Num is - NumSimetric.
 
 % counts the number of pieces for a given player
-count_pieces(GameState, PlayerColor, Count) :-
-    findall(_, get_piece(GameState, _-_, PlayerColor), Pieces),
+count_pieces(Board, PlayerColor, Count) :-
+    findall(_, get_piece(Board, _-_, PlayerColor), Pieces),
     length(Pieces, Count).
 
 % calculates the difference in the number of pieces between two players
-piece_difference(GameState, Player1Color, Player2Color, Difference) :-
-    count_pieces(GameState, Player1Color, Count1),
-    count_pieces(GameState, Player2Color, Count2),
+piece_difference(Board, Player1Color, Player2Color, Difference) :-
+    count_pieces(Board, Player1Color, Count1),
+    count_pieces(Board, Player2Color, Count2),
     Difference is Count1 - Count2.
 
 %generates moves for a given player
-generate_moves(GameState, Player, Move) :-
+generate_moves(Board, Player, Move) :-
     player_piece(Color, Player),
-    find_player_pieces(GameState, Color, Coordinates),
+    find_player_pieces(Board, Color, Coordinates),
     member(C1-L1, Coordinates),
-    length(GameState, Size),
+    length(Board, Size),
     between(1, Size, C2),
     between(1, Size, L2),
     dif(C1-L1, C2-L2),
     Move = C1-L1-C2-L2.
 
 %gets all valid moves for a given player
-valid_moves(GameState-Player-_-Variant, Moves) :-
+valid_moves(Board-Player-_-_, Moves) :-
     findall(Move, (
-        generate_moves(GameState, Player, Move),
-        check_move(GameState-Player, Move)
+        generate_moves(Board, Player, Move),
+        check_move(Board-Player, Move)
     ), Moves).
 
